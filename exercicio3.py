33cbb78c910b862b75054f487e02417d3fe8e4c8
wvxbs <gabriel.ferreira7854@gmail.com>
2021-09-07 01:26:46 -0300

    exercicio2 done

---
+++ exercicio3.py
@@ -0,0 +1,7 @@
+def main():
+    valor_euro = 6.15
+    n_euros = float(input('Quantos euros você quer comprar? ')) #não é possivel multiplicar um valor int por float.
+    print(f'Você precisa de {valor_euro} * {n_euros} reais.'), #não é possivel concatenar float com string.
+
+
+main()
