1dc4098ccd4e1cd2b2da228f90818911b93f03ec
wvxbs <gabriel.ferreira7854@gmail.com>
2021-09-15 12:10:25 -0300

    .

--- exercicio2.py
+++ exercicio2.py
@@ -3,7 +3,7 @@ def Spacer():
 
 def main():
     Spacer()
-    print("Return é utilizdo para retornar um valor ao chamador da função")
+    print("Return é utilizdo para retornar um valor ao chamador da função e finalizar a execução da função")
     Spacer()
     print("Return atua de certa forma como o ponto final da função, depois de um return nenhum código poder ser executado")
     Spacer()
